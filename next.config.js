3958148c32a4898872f64669c73e12cf62888ce8
Hoang Phuc <hoangphucnaht@gmail.com>
2024-05-01 22:17:59 +0700

    update NEXTAUTH_SECRET

--- next.config.js
+++ next.config.js
@@ -1,2 +1,6 @@
-const nextConfig = {}
-module.exports = nextConfig
+module.exports = {
+    env: {
+    NEXTAUTH_SECRET: 'Hoangphuc123',
+    MONGODB_URI:'mongodb+srv://phucvohoangbkcs:IArRTyqflZgEX5ny@cluster0.wrienzh.mongodb.net/dadn'    
+    },
+};
